50615670fa1e7e1e8a983e69e733caf327bb85bd
Akil Tajwar <akiltajwar@gmail.com>
2024-04-01 23:01:17 +0600

    signup page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -22,7 +22,7 @@ export const router = createBrowserRouter([
             {
                 path: '/signup',
                 element: <Signup/>
-            }
+            },
         ]
     },
 ]);
--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,38 +1,93 @@
-import { RiFileUserLine } from "react-icons/ri";
-import { PiShoppingBagOpenBold } from "react-icons/pi";
-import { TiTick } from "react-icons/ti";
+import { useState } from 'react';
+import { Link } from "react-router-dom";
+import Lottie from "lottie-react";
+import Employer from "../../../public/employer.json"
+import Jobseeker from "../../../public/jobseeker.json"
 
 const Signup = () => {
+    const [activeTab, setActiveTab] = useState(0);
+
     return (
-        <div className="lg:w-3/4 w-11/12 mx-auto lg:my-28 my-12">
+        <div className="lg:w-1/2 w-11/12 mx-auto lg:my-28 my-12">
             <div className="text-center pb-6">
-                <h1 className="text-5xl pb-2 font-bold text-[#004d70]">Get Started</h1>
-                <p className="text-lg font-semibold">In our website you can signup either as an employer or an jobseeker.</p>
+                <h1 className="text-5xl pb-2 font-bold">Get Started</h1>
+                <p className="text-lg font-semibold">In our website you can signup either as an employer or a jobseeker.</p>
             </div>
-            <div className="flex md:flex-row flex-col justify-center gap-6">
-                <div className="bg-[#edf7f4] p-3 w-[400px] custom-shadow">
-                    <h3 className="text-4xl text-center font-semibold text-[#318b9e]">Employer</h3>
-                    <div className="text-9xl py-2 flex justify-center "><RiFileUserLine /></div>
-                    <button className="w-full bg-[#1d9cb5] rounded text-white text-2xl p-1">I want to hire</button>
-                    <ul className="text-lg font-semibold pt-3">
-                        <li className="flex items-center"><TiTick className="absolute bg-[#1d9cb5] text-white rounded-full" /><span className="pl-8">Post Jobs</span></li>
-                        <li className="flex items-center"><TiTick className="absolute bg-[#1d9cb5] text-white rounded-full" /><span className="pl-8">Message Applicants</span></li>
-                        <li className="flex items-center"><TiTick className="absolute bg-[#1d9cb5] text-white rounded-full" /><span className="pl-8">Hire staff</span></li>
-                        <li className="flex items-center"><TiTick className="absolute bg-[#1d9cb5] text-white rounded-full" /><span className="pl-8">Pay your Staff</span></li>
-                    </ul>
-                </div>
-                <div className="bg-[#edf7f4] p-3 w-[400px] custom-shadow">
-                    <h3 className="text-4xl text-center font-semibold text-[#318b9e]">Jobseeker</h3>
-                    <div className="text-9xl py-2 flex justify-center"><PiShoppingBagOpenBold /></div>
-                    <button className="w-full bg-[#1d9cb5] rounded text-white text-2xl p-1">I want a job</button>
-                    <ul className="text-lg font-semibold pt-3">
-                        <li className="flex items-center"><TiTick className="absolute bg-[#1d9cb5] text-white rounded-full" /><span className="pl-8">Find online jobs</span></li>
-                        <li className="flex items-center"><TiTick className="absolute bg-[#1d9cb5] text-white rounded-full" /><span className="pl-8">Apply for jobs</span></li>
-                        <li className="flex items-center"><TiTick className="absolute bg-[#1d9cb5] text-white rounded-full" /><span className="pl-8">Submit your project</span></li>
-                        <li className="flex items-center"><TiTick className="absolute bg-[#1d9cb5] text-white rounded-full" /><span className="pl-8">Get paid</span></li>
-                    </ul>
-                </div>
+            <div className="flex justify-center gap-8">
+                <button
+                    className={`cursor-pointer text-2xl font-semibold focus:outline-none ${activeTab === 0 ? 'text-[#1d9cb5]' : 'text-gray-500'}`}
+                    onClick={() => setActiveTab(0)}
+                >
+                    I want to hire
+                </button>
+                <button
+                    className={`cursor-pointer text-2xl font-semibold focus:outline-none ${activeTab === 1 ? 'text-[#1d9cb5]' : 'text-gray-500'}`}
+                    onClick={() => setActiveTab(1)}
+                >
+                    I want a job
+                </button>
             </div>
+            {activeTab === 0 ?
+                <div className="flex justify-between gap-20 md:py-36 py-12">
+                    <div className="w-full md:block hidden">
+                        <Lottie animationData={Employer}/>
+                    </div>
+                    <div className='w-[600px] bg-[#edf7f4] custom-shadow rounded-lg p-8 mb-4'>
+                        <h2 className='text-xl text-center font-semibold mb-3 uppercase'>Employer Signup</h2>
+                        <form>
+                            <div className='flex flex-col gap-5'>
+                                <div className='pb-2'>
+                                    <label htmlFor="email">Email</label><br />
+                                    <input className='bg-[#f5f5f5] rounded p-2 border-slate-300 border w-full' type="email" name="email" id="" required />
+                                </div>
+                                <div className='pb-2'>
+                                    <label htmlFor="password">Password</label><br />
+                                    <input className='bg-[#f5f5f5] rounded p-2 border-slate-300 border w-full' type="password" name="password" id="" required />
+                                </div>
+                                <div className='pb-2'>
+                                    <label htmlFor="password">Confirm Password</label><br />
+                                    <input className='bg-[#f5f5f5] rounded p-2 border-slate-300 border w-full' type="password" name="password" id="" required />
+                                </div>
+                                <div className='flex gap-2 mb-3'>
+                                    <button className='w-full bg-[#1d9cb5] rounded text-white font-semibold p-2 mt-3'>Login</button>
+                                </div>
+                            </div>
+                        </form>
+                        <div className='text-center'>
+                            <small>Already have an account? <Link to='/login' className='text-[#1d7edd] font-semibold'>Signup now</Link></small>
+                        </div>
+                    </div>
+                </div> :
+                <div className="flex justify-between items-center gap-20 md:py-36 py-12">
+                    <div className="w-full md:block hidden">
+                        <Lottie animationData={Jobseeker}/>
+                    </div>
+                    <div className='w-[600px] bg-[#edf7f4] custom-shadow rounded-lg p-8 mb-4'>
+                        <h2 className='text-xl text-center font-semibold mb-3 uppercase'>Jobseeker Signup</h2>
+                        <form>
+                            <div className='flex flex-col gap-5'>
+                                <div className='pb-2'>
+                                    <label htmlFor="email">Email</label><br />
+                                    <input className='bg-[#f5f5f5] rounded p-2 border-slate-300 border w-full' type="email" name="email" id="" required />
+                                </div>
+                                <div className='pb-2'>
+                                    <label htmlFor="password">Password</label><br />
+                                    <input className='bg-[#f5f5f5] rounded p-2 border-slate-300 border w-full' type="password" name="password" id="" required />
+                                </div>
+                                <div className='pb-2'>
+                                    <label htmlFor="password">Confirm Password</label><br />
+                                    <input className='bg-[#f5f5f5] rounded p-2 border-slate-300 border w-full' type="password" name="password" id="" required />
+                                </div>
+                                <div className='flex gap-2 mb-3'>
+                                    <button className='w-full bg-[#1d9cb5] rounded text-white font-semibold p-2 mt-3'>Login</button>
+                                </div>
+                            </div>
+                        </form>
+                        <div className='text-center'>
+                            <small>Already have an account? <Link to='/login' className='text-[#1d7edd] font-semibold'>Signup now</Link></small>
+                        </div>
+                    </div>
+                </div>}
         </div>
     );
 };
