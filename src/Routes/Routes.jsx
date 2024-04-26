f77e873b3cde23cf441e8752694cef5262f71dfd
RaW <akram.iiuc.ctg@gmail.com>
2024-04-26 18:21:03 +0600

    Full code Updated

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -31,7 +31,7 @@ export const router = createBrowserRouter([
       },
       {
         path: '/user/:email',
-        element: <UserProfile />
+        element: <PrivateRouter><UserProfile /></PrivateRouter>
       },
       {
         path: "/findJob",
